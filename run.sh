0da1d2ba5b12f2e7bbee890e6baa121198f341a4
Rafael <rafaelso@stanford.edu>
2023-07-17 00:03:31 -0700

    commit issue with run.sh

--- run.sh
+++ run.sh
@@ -45,25 +45,10 @@ handle_interrupt() {
 
 # Execute the Python scripts with different configurations
 execute_script train.py --model="UNet_Film" --dataset="Sinusoidal_dataset_5_episodes.zarr.zip" \
- --batch_size=16 --obs_horizon=10 --pred_horizon=20 --inpaint_horizon=5
+ --batch_size=16 --obs_horizon=10 --pred_horizon=10 --inpaint_horizon=10
 
 execute_script train.py --model="UNet_Film" --dataset="Sinusoidal_dataset_5_episodes.zarr.zip" \
  --batch_size=16 --obs_horizon=10 --pred_horizon=20 --inpaint_horizon=10
 
 execute_script train.py --model="UNet_Film" --dataset="Sinusoidal_dataset_5_episodes.zarr.zip" \
  --batch_size=16 --obs_horizon=20 --pred_horizon=40 --inpaint_horizon=10
-
-execute_script train.py --model="UNet_Film" --dataset="TestingActions_dataset_5_episodes_3_modes.zarr.zip" \
- --batch_size=16 --obs_horizon=10 --pred_horizon=20 --inpaint_horizon=5
-
-execute_script train.py --model="UNet_Film" --dataset="TestingActions_dataset_5_episodes_3_modes.zarr.zip" \
- --batch_size=16 --obs_horizon=10 --pred_horizon=20 --inpaint_horizon=10
-
-execute_script train.py --model="UNet_Film" --dataset="TestingActions_dataset_5_episodes_3_modes.zarr.zip" \
- --batch_size=16 --obs_horizon=20 --pred_horizon=40 --inpaint_horizon=10
-
-execute_script train.py --model="UNet_Film" --dataset="Sinusoidal_dataset_5_episodes.zarr.zip" \
- --batch_size=16 --obs_horizon=10 --pred_horizon=20 --inpaint_horizon=10 --noise_scheduler='linear_v2'
-
-execute_script train.py --model="UNet_Film" --dataset="Sinusoidal_dataset_5_episodes.zarr.zip" \
- --batch_size=16 --obs_horizon=10 --pred_horizon=20 --inpaint_horizon=10 --noise_scheduler='linear'
