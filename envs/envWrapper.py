7eb1713ce6af0d5910db133d59c21afd752485da
Rafael <rafaelso@stanford.edu>
2023-07-18 00:04:21 -0700

    modified wrapper

--- envs/envWrapper.py
+++ envs/envWrapper.py
@@ -28,17 +28,52 @@ class EnvWrapper(CarRacing):
         return info
     
     def reset_car(self, x, y, xdot, ydot, initAngle):
-        state = super().reset()
+        # state = super().reset()
         
 
-        init_angle =  initAngle #np.arctan2(ydot, xdot) - np.pi / 2
-        self.car = Car(self.world, init_angle=float(init_angle), init_x= float(x), init_y=float(y))
-        self.car.hull.linearVelocity = Box2D.b2Vec2(float(xdot),float(ydot))
-        v = float(np.sqrt(xdot**2 + ydot**2))
+        # init_angle =  initAngle #np.arctan2(ydot, xdot) - np.pi / 2
+        # self.car = Car(self.world, init_angle=float(init_angle), init_x= float(x), init_y=float(y))
+        # self.car.hull.linearVelocity = Box2D.b2Vec2(float(xdot),float(ydot))
+        # v = float(np.sqrt(xdot**2 + ydot**2))
         
-        for idx, w in enumerate(self.car.wheels):
+        # for idx, w in enumerate(self.car.wheels):
+        #     w.angularVelocity = v
+        #     w.omega = v / (w.wheel_rad)
+
+                # Delete all points in lists
+        self.t1.clear()
+        self.t2.clear()
+        self.t3.clear()
+        self.t4.clear()
+        self.t5.clear()
+        
+        self._destroy()
+        self.reward = 0.0
+        self.prev_reward = 0.0
+        self.tile_visited_count = 0
+        self.t = 0.0
+        self.road_poly = []
+
+        while True:
+            success = self._create_track()
+            if success:
+                break
+            if self.verbose == 1:
+                print(
+                    "retry to generate track (normal if there are not many"
+                    "instances of this message)"
+                )
+        
+
+        init_angle = initAngle
+        self.car = Car(self.world, init_angle=float(init_angle), init_x= float(x), init_y=float(y))
+
+        self.car.hull.linearVelocity = Box2D.b2Vec2(float(xdot),float(ydot))
+        v = float(np.sqrt(float(xdot)**2 + float(ydot)**2))    
+        for w in self.car.wheels:
             w.angularVelocity = v
             w.omega = v / (w.wheel_rad)
+        return self.step(None)[0]
 
         
     def close(self):
